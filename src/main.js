08d621ba9afa0a6df2df433abfede568f50dafe9
gamnt-0106 <gamntph14216@fpt.edu.vn>
2022-01-10 21:15:13 +0700

    first commit

---
+++ src/main.js
@@ -0,0 +1,5 @@
+// import Navigo from "navigo"; 12.3k (gripped:4.2k)
+
+// const roter  = new Navigo("/", {LinkSelector:"a"});
+// roter.on {(
+//   //neu user truy cap vao duong dan / thi se lam viec gi do
